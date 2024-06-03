f9947c5b40e12bfe6a4c50fc40f18e1a9fa6b007
tsrithvik <sairithvik1234@gmail.com>
2024-06-03 23:06:17 +0530

    font of banner text is modified

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,11 +3,16 @@ import { Container, Row, Col } from 'react-bootstrap';
 import { ArrowRight } from 'react-bootstrap-icons';
 import profile from "../images/profile2.jpeg";
 
+// Importing the font using a CDN
+const FontImport = () => (
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet" />
+);
+
 export const Banner = () => {
     const [text, setText] = useState('');
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const [delta, setDelta] = useState(300); // Define delta and setDelta here
+    const [delta, setDelta] = useState(300);
     const toRotate = ["Electronics Engineer", "Programmer", "Tech Enthusiast"];
     const period = 2000;
 
@@ -29,25 +34,26 @@ export const Banner = () => {
             } else if (isDeleting && updateText === '') {
                 setIsDeleting(false);
                 setLoopNum(loopNum + 1);
-                setDelta(500);
+                setDelta(200);
             }
         };
 
         const ticker = setInterval(tick, delta);
 
         return () => clearInterval(ticker);
-    }, [text, loopNum, isDeleting, delta]); // Add delta to the dependency array
+    }, [text, loopNum, isDeleting, delta]);
 
     return (
         <section className="banner" id="home">
+            <FontImport />
             <Container>
                 <Row className="align-items-centre">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline"> Welcome to my Portfolio</span>
-                        <h1>{`Hi I'm Sai Rithvik `}<span className="wrap"> <br />{text}</span></h1>
+                        <h1 style={{ fontFamily: 'Bebas Neue, cursive', color: 'rgb(37, 150, 190)'}}>{`Hi, I'm Sai Rithvik `}<span className="wrap"> <br />{text}</span></h1>
                         <p>Welcome to my Portfolio! I'm enthusiastic about connecting with professionals like you and sharing experiences 🤝.
                         My expertise lies in web development and various programming languages, where I've successfully delivered projects ✌️. 
-        I'm open to new opportunities and collaborations, feel free to reach out. I'm eager to explore how we can work together😊.
+             I'm open to new opportunities and collaborations, feel free to reach out. I'm eager to explore how we can work together😊.
                         </p>
                         <a href="https://www.linkedin.com/in/tsairithvik" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
                             <button onClick={() => console.log('connect')}>
